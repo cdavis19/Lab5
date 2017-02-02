84534dc94dcb59d876285e536dbc2b89ba8888e0
cdavis19 <cjdavis6791@gmail.com>
2017-02-02 15:03:24 -0500

    initial

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 var temperatureInF = prompt("convert");
 var temperatureInC = (temperatureInF-32) * 5/9;
 console.log("the temperature is " + temperatureInC + " c");
+
+var temperatureInC = prompt("convert");
+var temperatureInF = (temperatureInC * 9/5) + 32;
+console.log("the temperature is " + temperatureInF + " F");
